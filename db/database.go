28636dbeddc84781edc390af7ac6b5f6384e0a9e
shay_soifer <shay_soifer@dell.com>
2022-07-10 16:42:39 +0300

    add pg driver

--- db/database.go
+++ db/database.go
@@ -8,6 +8,8 @@ import (
 	"github.com/golang-migrate/migrate"
 	"github.com/golang-migrate/migrate/database"
 	"github.com/golang-migrate/migrate/database/postgres"
+	_ "github.com/golang-migrate/migrate/source/file"
+	_ "github.com/lib/pq"
 	"log"
 	"reflect"
 	"strconv"
